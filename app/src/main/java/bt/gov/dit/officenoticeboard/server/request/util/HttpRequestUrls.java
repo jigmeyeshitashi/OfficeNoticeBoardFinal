b670a228fe4187e374dbea49ce8e29fa2b293c77
jigmeyeshitashi <ypeldon@dagana.gov.bt>
2017-05-02 17:38:02 +0600

    Edited Login from "email id " to "employee id"

--- app/src/main/java/bt/gov/dit/officenoticeboard/server/request/util/HttpRequestUrls.java
+++ app/src/main/java/bt/gov/dit/officenoticeboard/server/request/util/HttpRequestUrls.java
@@ -6,6 +6,6 @@ package bt.gov.dit.officenoticeboard.server.request.util;
 
 public final class HttpRequestUrls {
     //public static final String BASE_URL = "http://192.168.43.104/officeNoticeBoard/webservices/";
-    public static final String BASE_URL = "http://192.168.3.73/officeNoticeBoard/webservices/";
+    public static final String BASE_URL = "http://192.168.3.76/officeNoticeBoard/webservices/";
     public static final String LOGIN_URL = BASE_URL + "login.php?employee_id=%s&password=%s";
 }
